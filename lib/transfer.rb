bed86f857d601cae9c3866be8d68ccca41e82930
Anu Khambete <anukhambete@gmail.com>
2018-05-03 18:02:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,6 +21,9 @@ class Transfer
     if @status == "pending"
       if self.valid? == true
         if @sender.balance > @amount
+          @sender.balance = @sender.balance - @amount
+          @receiver.balance = @receiver.balance + @amount
+          @status = ""
   end
   
 end
