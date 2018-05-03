ab28ea6a8181267433fff5b245c867f7c301562e
Anu Khambete <anukhambete@gmail.com>
2018-05-03 18:29:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -41,6 +41,7 @@ class Transfer
   def reverse_transfer
     
     if @status = "complete"
+      
       @sender.balance = @sender.balance + @amount
       
       @receiver.balance = @receiver.balance - @amount
